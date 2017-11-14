d6d4cf5f4b4338ccae6b88b3b869dac873dafd98
Michael Tiller <michael.tiller@gmail.com>
2017-11-14 04:56:34 -0500

    General cleanup
    Prelude to getting a bit more organized.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { Router, Link } from 'react-static'
 //
 import Routes from 'react-static-routes'
+
 const debug = require('debug');
 const appDebug = debug("mbe:static-app");
-appDebug.enabled = true;
+// appDebug.enabled = true;
 
 import './app.css'
 
--- src/containers/404.tsx
+++ src/containers/404.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 
 export default () => (
   <div>
-    <h1>404 - Oh no's! We couldn't find that page :(</h1>
+    <h1>Unable to find requested page.</h1>
+    <p>Return to the <a href="/">Table of Contents</a></p>
   </div>
 )
